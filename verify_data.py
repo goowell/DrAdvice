fb64689c44476be4cb60d8689f70d86a6dbe8961
Zhang Shanwei <Shanwei.Zhang@JOT.local>
2017-04-04 14:50:53 +0800

    add function to verify data

---
+++ verify_data.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+def main():
+    from datetime import datetime
+
+    start = datetime.now()
+    print('hello..')
+
+    client = MongoClient('192.168.2.110')
+    collection = client.xinhuahos.paients
+
+    for d in collection.find():
+        info = d.get('d').get('info')
+        if len(info) != 12 and info[0] != '1':
+            print('invalid patient info:' + d['_id'], len(info))
+        if len(d.get('d').get('doctor_advice')) == 0:
+            print('invalid doctor advice:' + d['_id'])
+    client.close()
+
+    print(datetime.now() - start)
+
+
+if __name__ == '__main__':
+    main()
