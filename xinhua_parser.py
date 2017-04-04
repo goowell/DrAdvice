a99ec6e9e50cf80dc5a0e6a86a97f4088b95e475
Zhang Shanwei <Shanwei.Zhang@JOT.local>
2017-04-04 17:40:02 +0800

    change to concurrent process

--- xinhua_parser.py
+++ xinhua_parser.py
@@ -4,6 +4,7 @@ from html.parser import HTMLParser
 from concurrent.futures import ThreadPoolExecutor
 from pymongo import MongoClient
 
+
 class MyHtmlParser(HTMLParser):
 
     def init(self):
@@ -34,7 +35,7 @@ class MyHtmlParser(HTMLParser):
             self.profile_keyword_open = True
         elif data == self.dadvice_keyword:
             self.dadvice_keyword_open = True
-            
+
     def handle_starttag(self, tag, attrs):
         if self.div_open:
             if tag == 'div':
@@ -57,13 +58,13 @@ class MyHtmlParser(HTMLParser):
             self.profile_open = False
             self.profile_keyword_open = False
 
-        elif  self.div_open:
+        elif self.div_open:
             is_div = tag == "div"
             if is_div and self.div_count > 0:
                 self.div_count -= 1
             elif is_div:
                 self.div_open = False
-                
+
             elif tag == 'tr':
                 self.doctor_advice.append(self.single_advie)
                 self.single_advie = []
@@ -75,12 +76,13 @@ class MyHtmlParser(HTMLParser):
         elif tag == 'td' and self.profile_open and self.td_open:
             self.profile.append('')
             self.td_open = False
-                
+
     def check_style(self, attrs, style):
         for attr in attrs:
             if attr[0] == 'class' and attr[1] == style:
                 return True
         return False
+
 
 def parse_one_file(file_path):
     parser = MyHtmlParser()
@@ -93,14 +95,15 @@ def parse_one_file(file_path):
         s1 = c[:idx]
         idx2 = c.rfind('停止医生')
         s2 = c[idx2:]
-        parser.feed(s1+s2)
+        parser.feed(s1 + s2)
     return parser.profile, parser.doctor_advice
+
 
 def parse_one(dir, file_name):
     a, b = parse_one_file(dir + file_name)
     c, d = parse_one_file(dir + file_name.replace('.', 'S.'))
     b.extend(d)
-    return {'_id':file_name.split('.')[0],'d':{'info':a, 'doctor_advice': b}}
+    return {'_id': file_name.split('.')[0], 'd': {'info': a, 'doctor_advice': b}}
 
 
 def main():
@@ -112,12 +115,19 @@ def main():
     all_html = [f for f in listdir(dir) if f.endswith('htm')and len(f) == 10]
     client = MongoClient('192.168.2.110')
     collection = client.xinhuahos.paients
-    collection.save(parse_one(dir, 'B30827.htm'))
+    # collection.save(parse_one(dir, 'B30827.htm'))
     # collection.save(parse_one(dir, 'B31008.htm'))
 
+    def _save(f): 
+        print(f)
+        return collection.save(parse_one(dir, f))
     # for f in all_html:
     #     collection.save(parse_one(dir, f))
     #     print(f)
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        fs = executor.map(_save, all_html)
+        list(fs)
 
     client.close()
 
