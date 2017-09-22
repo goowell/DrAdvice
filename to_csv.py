22b7ed30fb3ed1944d4d4e81d3df3110c4a9d670
Zhang Shanwei <Shanwei.Zhang@jotautomation.com>
2017-09-22 20:07:05 +0800

    parsed one person

--- to_csv.py
+++ to_csv.py
@@ -1,9 +1,9 @@
 from db import paients_calculated
 from logger import logger
+from datetime import datetime
 
 def main():
     'main entry'
-    from datetime import datetime
     start = datetime.now()
     logger.info('hello..')
     toCsv()
@@ -17,11 +17,46 @@ def toCsv():
     counter = 1
     for p in paients_calculated.find():
         print(p)
-        a= p_info.format(counter,p['info'][2],p['_id'],p['info'][5],p['info'][7],'#'.join(p['info']))
-        
-        print(a)
+        info= p_info.format(counter,p['info'][2],p['_id'],p['info'][5],p['info'][7],'#'.join(p['info']))
+        nu_counter = 0
+        len_nu = len(p['nu'])
+        while nu_counter < len_nu:
+            nu = p['nu'][nu_counter]
+            date_nu = nu['d']
+            en_nu,pn_nu = 0,0
+
+            if nu['en']:
+                en_nu = nu['v']
+            else:
+                pn_nu = nu['v']
+            if (nu_counter < len_nu - 1):
+                next_nu = p['nu'][nu_counter+1]
+                if next_nu['d'] == date_nu:
+                    if next_nu['en']:
+                        en_nu = next_nu['v']
+                    else:
+                        pn_nu = next_nu['v']
+                    nu_counter += 1
+            nu_counter += 1
+            wight = _get_wight(p, date_nu)
+
+            one_nu = daily_info.format(_total_days(p, date_nu), date_nu,wight,en_nu,pn_nu)
+            print(one_nu)
+        print(info)
         counter += 1
         break
+
+def _get_wight(p, date_nu):
+    wight = 0
+    for w in p['wt']:
+        if date_nu > w['st']:
+            wight = w['w']
+        else:
+            break
+    return wight
+
+def _total_days(p, date_nu):
+    return (datetime.strptime(date_nu,'%Y-%m-%d')-datetime.strptime(p['info'][7].split(' ')[0],'%Y-%m-%d')).days
 
 if __name__ == '__main__':
     main()
