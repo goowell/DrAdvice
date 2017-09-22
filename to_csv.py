8f14ecbcd6372247d10e98659e13b6b9d798c1eb
Zhang Shanwei <Shanwei.Zhang@jotautomation.com>
2017-09-22 23:26:43 +0800

    version 1.0

--- to_csv.py
+++ to_csv.py
@@ -11,13 +11,16 @@ def main():
     print('Elapsed time: ', datetime.now() - start)
 
 def toCsv():
+    f = open('data.csv',mode='w',encoding='utf-8')
     header = '序号,姓名,住院号,入科年龄,入科日期,入科天数,日期,体重,EN,PN,其他\n'
     p_info = '{0},{1},{2},{3},{4},,,,,,{5}\n'
     daily_info = ',,,,,{0},{1},{2},{3},{4},\n'
     counter = 1
+    f.write(header)
     for p in paients_calculated.find():
-        print(p)
+        # print(p)
         info= p_info.format(counter,p['info'][2],p['_id'],p['info'][5],p['info'][7],'#'.join(p['info']))
+        f.write(info)
         nu_counter = 0
         len_nu = len(p['nu'])
         while nu_counter < len_nu:
@@ -41,11 +44,9 @@ def toCsv():
             wight = _get_wight(p, date_nu)
 
             one_nu = daily_info.format(_total_days(p, date_nu), date_nu,wight,en_nu,pn_nu)
-            print(one_nu)
-        print(info)
+            f.write(one_nu)
         counter += 1
-        break
-
+    f.close()
 def _get_wight(p, date_nu):
     wight = 0
     for w in p['wt']:
